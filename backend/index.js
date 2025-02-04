70897b4cca271e3760d27455027bfd997f320f09
Klevis <klevi.dervishi@gmail.com>
2025-02-04 20:15:08 +0200

    Initiated the backend and the index file for the node js

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+
+app.get('/',(req, res)=>{
+    return res.send("Hello World")
+})
+
+app.listen(5000,()=>{
+    console.log('Server started!')
+})
