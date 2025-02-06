8513e9f75b4f93ebfc8156f8abc5d5790744a070
Klevis <klevi.dervishi@gmail.com>
2025-02-06 14:36:01 +0200

    Creating the routes file for the project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css'
-
+import {BrowserRouter, Routes, Route} from 'react-router'
+import Home from './components/home/home'
+//all the routes of the project will go under this file 
+//follow the same structure
 function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+         <Route path='/' element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
