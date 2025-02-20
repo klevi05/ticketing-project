c112d8975b7978339158b32e5089e239dc30be21
Klevis <klevi.dervishi@gmail.com>
2025-02-20 14:20:13 +0200

    some chagnes

--- frontend/src/components/signup/signup.tsx
+++ frontend/src/components/signup/signup.tsx
@@ -1,26 +1,30 @@
 import TextField from '@mui/material/TextField';
 import Navbar from '../navbar/navbar'
+import Alert from '@mui/material/Alert';
 import './signup.css'
 import { useState } from 'react';
 import { useNavigate } from 'react-router';
 function Signup(){
     const navigate = useNavigate()
+    const [alert, setAlert] = useState('')
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [team, setTeam] = useState('');
-    function handleSignUp(e){
-        e.preventDefault();
+    function handleSignUp(){
         if(name && email && password && team != ''){
             console.log('sent')
             navigate('/',{replace: true})
-        } 
+        } else{
+            setAlert('One of the fields is empty!')
+        }
     }
     return(
         <>
         <div>
             <Navbar/>
             <div className='signupPage'>
+                {alert==='' ? <><p></p></> :<Alert style={{marginBottom: '1rem'}} severity="warning">{alert}</Alert>}
                 <div className='signupForm'>
                     <h4>Sign Up</h4>
                     <TextField
@@ -28,6 +32,7 @@ function Signup(){
                     label='Name'
                     variant="standard" 
                     type='text'
+                    required
                     onChange={(e)=>{setName(e.target.value)}}
                     />
                     <TextField 
@@ -35,6 +40,7 @@ function Signup(){
                     label="Email" 
                     type='email'
                     variant="standard"
+                    required
                     onChange={(e)=>{setEmail(e.target.value)}}
                     />
                     <TextField 
@@ -42,6 +48,7 @@ function Signup(){
                     label="Password"
                     type='password' 
                     variant="standard"
+                    required
                     onChange={(e)=>{setPassword(e.target.value)}}
                     />
                     <TextField 
@@ -49,9 +56,10 @@ function Signup(){
                     label="Team" 
                     variant="standard"
                     type='text'
+                    required
                     onChange={(e)=>{setTeam(e.target.value)}}
                     />
-                    <button onClick={(e)=>{handleSignUp(e)}} className='signupButton'>Submit</button>
+                    <button onClick={handleSignUp} className='signupButton'>Submit</button>
                 </div>
             </div>
         </div>
