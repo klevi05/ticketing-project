f0855e32cd5f0bffd240a7084ce86197ca203809
Klevis <klevi.dervishi@gmail.com>
2025-04-06 22:36:02 +0300

    Fixed the limit of 10 for tickets and some style things

--- frontend/src/components/home/home.tsx
+++ frontend/src/components/home/home.tsx
@@ -117,7 +117,7 @@ function Home(){
                             <h1 className='team'>Team: {tickets['team']}</h1>
                             <button onClick={()=>{setEdit(true); setTicketData(tickets);}} className='threeDots'><svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="black"><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg></button>
                             </div>
-                            <h4>Subject: {tickets['subject']}</h4>
+                            <h4 className='subject'>Subject: {tickets['subject']}</h4>
                             <h4>Status: {tickets['status']}</h4>
                             <h4>{tickets['updatedAtFormatted']} {tickets['updatedDateFormatted']}</h4>
                             <div className='ticketActions'>
